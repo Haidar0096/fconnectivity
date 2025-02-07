512e5505ccefe5fdc2fd4e253f80f718f282c745
Haidar Mehsen <49842031+Haidar0096@users.noreply.github.com>
2025-02-07 14:25:30 +0200

    Development (#1)
    * Updated dependencies
    
    * Example and docs enhancements
    
    * Docs enhancements
    
    * Example refactor
    
    * Example refactor
    
    * Added safety cancels for the timers and stream subscriptions
    
    * Enhanced example
    
    * Enhanced example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,43 +1,118 @@
 import 'package:fconnectivity/fconnectivity.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Change the interval of internet access check to 2 seconds.
+  InternetAccessChecker.instance.setInternetAccessCheckInterval(
+    const Duration(milliseconds: 2000),
+  );
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final TextEditingController _intervalController = TextEditingController();
+
+  @override
+  void dispose() {
+    _intervalController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              InternetAccessConsumer(
-                onInternetAccessGained: (BuildContext context) => debugPrint(
-                  'Internet access gained (logged by InternetAccessConsumer)',
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InternetAccessConsumer(
+                  onInternetAccessGained: (BuildContext context) => debugPrint(
+                    'Internet access gained (logged by InternetAccessConsumer)',
+                  ),
+                  onInternetAccessLost: (BuildContext context) => debugPrint(
+                    'Internet access lost (logged by InternetAccessConsumer)',
+                  ),
+                  builder: ({
+                    required BuildContext context,
+                    required bool hasInternetAccess,
+                  }) =>
+                      ColoredBox(
+                    color: Colors.yellow,
+                    child: RichText(
+                      text: TextSpan(
+                        style: const TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                        text:
+                            'This text widget is wrapped with InternetAccessConsumer, so '
+                            'it listens to internet access states, and also it '
+                            'rebuilds when internet access state changes.'
+                            '\nCurrent state of internet access:\n',
+                        children: [
+                          TextSpan(
+                            text: hasInternetAccess
+                                ? 'Available'
+                                : 'Not Available',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.red),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-                onInternetAccessLost: (BuildContext context) => debugPrint(
-                  'Internet access lost (logged by InternetAccessConsumer)',
+                const SizedBox(height: 20),
+                InternetAccessListener(
+                  onInternetAccessGained: (BuildContext context) => debugPrint(
+                    'Internet access gained (logged by InternetAccessListener)',
+                  ),
+                  onInternetAccessLost: (BuildContext context) => debugPrint(
+                    'Internet access lost (logged by InternetAccessListener)',
+                  ),
+                  child: const ColoredBox(
+                    color: Colors.cyan,
+                    child: Text(
+                      'This text widget is wrapped with an InternetAccessListener, '
+                      'it listens to internet access states, but it does not '
+                      'rebuild when internet access state changes.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
                 ),
-                builder: ({
-                  required BuildContext context,
-                  required bool hasInternetAccess,
-                }) =>
-                    Text(
-                  'This text widget rebuilds when internet access state changes. Current state of internet access: ${hasInternetAccess ? 'Available' : 'Not Available'}',
-                  textAlign: TextAlign.center,
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _intervalController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter the interval in milliseconds',
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              InternetAccessListener(
-                onInternetAccessGained: (BuildContext context) => debugPrint(
-                  'Internet access gained (logged by InternetAccessListener)',
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    InternetAccessChecker.instance
+                        .setInternetAccessCheckInterval(
+                      Duration(
+                          milliseconds:
+                              int.tryParse(_intervalController.text) ?? 0),
+                    );
+                  },
+                  child: const Text('Change internet access check interval'),
                 ),
-                onInternetAccessLost: (BuildContext context) => debugPrint(
-                  'Internet access lost (logged by InternetAccessListener)',
-                ),
-                child: const Text(
-                  'This text widget does not rebuild when internet access state changes.',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+}
